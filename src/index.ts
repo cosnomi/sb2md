e605ae4db9f0804df6588b653515627eabe7f367
hogashi <azuma.ginkakuji@gmail.com>
2022-06-21 23:06:18 +0900

    require from dist to bin

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
 import { parse } from '@progfay/scrapbox-parser';
 import { pageToMarkdown } from './pageToMarkdown';
 
-process.stdin.setEncoding('utf8');
+export const run = () => {
+  process.stdin.setEncoding('utf8');
 
-const lines: string[] = [];
-const reader = require('readline').createInterface({
-  input: process.stdin,
-});
+  const lines: string[] = [];
+  const reader = require('readline').createInterface({
+    input: process.stdin,
+  });
 
-reader.on('line', (line: string) => {
-  lines.push(line);
-});
-reader.on('close', () => {
-  const markdown = pageToMarkdown(parse(lines.join('\n'), { hasTitle: false }));
-  console.log(markdown);
-});
+  reader.on('line', (line: string) => {
+    lines.push(line);
+  });
+  reader.on('close', () => {
+    const markdown = pageToMarkdown(
+      parse(lines.join('\n'), { hasTitle: false })
+    );
+    console.log(markdown);
+  });
+};
